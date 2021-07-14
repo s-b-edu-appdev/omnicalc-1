6974e6191f8923836e495ddb580838d10ecbc8cb
s-b-edu <sbaur@chicagobooth.edu>
2021-07-14 04:07:47 +0000

    72% done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,31 @@ class ApplicationController < ActionController::Base
         render({ :template => "calculation_templates/square_form.html.erb"})
     end
     def calculate_square
-        @num = params.fetch("number").to_i
+        @num = params.fetch("number").to_f
         @square_num = @num * @num
         render({ :template => "calculation_templates/square_results.html.erb"})
     end
     def blank_square_root_form
         render({ :template => "calculation_templates/square_root_form.html.erb"})
     end
+    def calculate_square_root
+        @num = params.fetch("number").to_f
+        @square_root_num = @num ** 0.5
+        render({ :template => "calculation_templates/square_root_results.html.erb"})
+    end
     def blank_payment_form
         render({ :template => "calculation_templates/payment_form.html.erb"})
     end
+    def calculate_payment
+        render({ :template => "calculation_templates/payment_results.html.erb"})
+    end
     def blank_random_form
         render({ :template => "calculation_templates/random_form.html.erb"})
+    end
+    def calculate_random
+        @low = params.fetch("user_min").to_f
+        @high = params.fetch("user_max").to_f
+        @rand_num = rand(@low .. @high)
+        render({ :template => "calculation_templates/random_results.html.erb"})
     end
 end
